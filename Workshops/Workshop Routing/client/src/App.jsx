c754fb95a155568189bbb2927cf14408bf88dd0f
Krasimira Valcheva <krasimira.valcheva88@gmail.com>
2023-11-15 15:04:23 +0200

    Add header component

--- Workshops/Workshop Routing/client/src/App.jsx
+++ Workshops/Workshop Routing/client/src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import Header from './components/Header/Header'
 
 function App() {
 
   return (
     <>
-     
+      <div id="box">
+        <Header></Header>
+      </div>
     </>
   )
 }
