036d6f741574178140ae599c03cb48e5a1b7d508
Krasimira Valcheva <krasimira.valcheva88@gmail.com>
2023-11-20 11:25:17 +0200

    Add Login page

--- Workshops/Workshop Routing/client/src/App.jsx
+++ Workshops/Workshop Routing/client/src/App.jsx
@@ -4,6 +4,8 @@ import Header from './components/Header/Header'
 import Home from './components/Home/Home'
 import GameList from './components/GameList/GameList'
 import GameCreate from './components/GameCreate/GameCreate'
+import Register from './components/Register/Register'
+
 
 function App() {
 
@@ -16,6 +18,9 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/allGames" element={<GameList />} />
           <Route path="/createGame" element={<GameCreate />} />
+          <Route path="/register" element={<Register />} />
+
+
         </Routes>
 
       </div>
