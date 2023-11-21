0a5bbfaa5af4d795b3f610040ac0358a6a444118
Krasimira Valcheva <krasimira.valcheva88@gmail.com>
2023-11-21 10:10:31 +0200

    Create to do page

---
+++ Demoes/Components - Deep Dive/client/src/components/Todo.js
@@ -0,0 +1,17 @@
+export default function Todo({
+    _id,
+    text,
+    isCompleted,
+    toggleTodoStatus
+}) {
+
+    return (
+        <tr className={`todo ${isCompleted ? 'is-completed' : ''}`.trim()}>
+            <td>{text}</td>
+            <td>{isCompleted ? 'Complete' : 'Not Complete'}</td>
+            <td className="todo-action">
+                <button className="btn todo-btn" onClick={() => toggleTodoStatus(_id)}>Change status</button>
+            </td>
+        </tr>
+    )
+}
